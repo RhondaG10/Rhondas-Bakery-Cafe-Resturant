e5090737e4085f20d1ce1f0bdc6fc708db665e6b
RhondaG10 <rhondaginsures@gmail.com>
2021-07-01 14:41:09 -0400

    added store offer and javascript coding

--- js/bakery.js
+++ js/bakery.js
@@ -61,3 +61,70 @@ function createListItems(arr) {
 // Insert foodlist into the main element in the menu section content in index.html file
 const foodItems = createListItems(foodlist);
 document.querySelector("main").innerHTML = foodItems;
+
+// JavaScript for store offer
+// Declare variables, today stores todays date.
+let weekFromToday, day, date, month, year, monthNames, dayNames;
+let storeOffer = "";
+// creates a new date object with the current date and time:
+let today = new Date();
+
+// Create arrays to hold the names of months and days
+monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+// Add 7 days time (added in milliseconds) for the store offer:
+// minutes = 60 * 1000
+// hours = minutes * 60
+// days = hours * 24
+// weekFromToday = using getTime() method produces the store offer period 7 days time (added in milliseconds) today.getime() returns today's date + 7 days.
+weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+// Collect the parts of the date to show on the page
+
+//  The getDay() method returns the day of the week (from 0 to 6) for the specified date. Note: Sunday is 0, Monday is 1, and so on.
+day = dayNames[weekFromToday.getDay()];
+
+//  The getDate() method returns the day of the month (from 1 to 31) for the specified date.
+date = weekFromToday.getDate();
+
+// The getMonth() method returns the month (from 0 to 11) for the specified date, according to local time. Note: January is 0, February is 1, and so on.
+month = monthNames[weekFromToday.getMonth()];
+
+// The getFullYear() method returns the year (four digits for dates between year 1000 and 9999) of the specified date. For example 2021
+year = weekFromToday.getFullYear();
+
+// Create the message with the parts of the date to show on the page using template literal
+storeOffer = `<h4><span class="text-primary" style = 'color: #1414b0; font: italic bold 1.5rem "Raleway", Sans-Serif; letter-spacing: .1rem;'>20%</span> Off Pastry Items - Offer Expires Next:</h4>
+  <p class= "text-primary" style = 'letter-spacing: .1rem; font-weight: bold;'>(${day} ${month} ${date}, ${year} in 7 days!)</p>`;
+
+//The insertAdjacentHTML() method inserts the above text as HTML, into a specified position that being 'beforebegin' - This inserts the storeoffer text before the main element.
+
+document.querySelector("main").insertAdjacentHTML("beforeend", storeOffer);
+
+// Reload the web page every hour to update the store offer
+setInterval(() => {
+  location.reload();
+}, 1000 * 60 * 60);

16d5e3c16e2b32c53611b0575a0c826089769137
RhondaG10 <rhondaginsures@gmail.com>
2021-07-11 13:59:01 -0400

    updated readme file and code comments

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 This is a bakery cafe website.
 
-My CSS features are as follows:
-
 1. I have created a navigation menu that expands & reduces to accommodate different device sizes – mobile to desktop. I used a media query to accomplish this.
 2. I used media queries and Flexbox CSS to organize my content which allows it to adapt to different devices. It causes my content to flip or swap places, then finally to reduce to a single column when I size the page to mobile.
 
-My Javascript features are as follows:
+3. I used a JavaScript array to populate the initial food menu section - “Sweet Treats”, on the web page.
+4. I used JavaScript to control the hamburger icon. This causes it to show or hide the navigation items - “Home”, “Menu”, “About”, and “Contact”.
 
-1. I used a JavaScript array to populate the initial food menu section - “Sweet Treats”, on the web page.
-2. I used JavaScript to control the hamburger icon. This causes it to show or hide the navigation items - “Home”, “Menu”, “About”, and “Contact”.
+The following are my JavaScript features:
+
+1. I calculated and displayed the store offer based on the current date - I use that in the discount offer that expires seven days from the current date.
+
+2. I Created a month and day array and use these to populate and calulate the store offer days and dates.
+
+3. I show Louisville weather conditions by using the AccuWeather API. This is inserted into the webpage's main image.
+
+4. I used regex to validate and correctly display form name, email and telephone inputs. I use bootstrap tool tips as form field guides and alerts if fields are entered correctly or not.
--- js/api.js
+++ js/api.js
@@ -6,7 +6,7 @@ const icon = document.querySelector(".iconWeather img");
 
 const key = "gMWN0JqP9edRf3cAO5nByGloscV8LiyO";
 
-// funtion to update weather conditions and insert into the webpage
+// funtion to update weather conditions and insert the results into the webpage
 const updateWeather = (data) => {
   const weather = data;
   const htmlTemp = `
@@ -15,7 +15,8 @@ const updateWeather = (data) => {
   temp.innerHTML = htmlTemp;
 
   // Update icon images
-  // The AccuWeather's WeatherIcon property provides a unique number that we can match icon images to.
+  // weather.WeatherIcon produces a unique number corresponding to the weather conditions
+
   // const iconSrc = `img/imgIcon/icons/${weather.WeatherIcon}.svg`;
   const iconSrc = `img/imgIcon/icons/${weather.WeatherIcon}-s.png`;
 
@@ -26,15 +27,17 @@ const updateWeather = (data) => {
   // console.log(weather.WeatherIcon);
 };
 
-// get weather information
+// function to get weather information asynchronously
+// The async keyword is added to the function to tell it to return a Promise object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
 const getWeather = async () => {
   const base = `http://dataservice.accuweather.com/currentconditions/v1/348428?apikey=${key}`;
 
+  //The await keyword pauses your code until the promise fulfills, then returns the resulting value.
   // Wait until the promise has resolved and then get the data response object back and asign it to the response const variable
   const response = await fetch(base);
   console.log(response);
 
-  // Wait until the promise has resolved and then using the json() method get the response object, and then pass or convert that into a javascript array object and store that in const data
+  // Wait until the promise has resolved and then using the json() method get the response object, and then pass or convert that into a javascript array object and store that in the const variable called data.
   const data = await response.json();
   console.log(data);
   return data[0];
